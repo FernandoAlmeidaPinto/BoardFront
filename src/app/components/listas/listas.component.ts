f1d2ff215153b0170c30b2254a1bff7bafae7b63
Fernando Almeida Pinto <fernando.almeida.pinto@gmail.com>
2022-05-28 00:08:11 -0300

    definindo criação de label

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ILista, IProjeto } from 'src/app/service/boards.service';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { BoardsService, IBoard, ILabel, ILista, IProjeto } from 'src/app/service/boards.service';
 import { CardService } from 'src/app/service/card.service';
 import { ListaService } from 'src/app/service/lista.service';
+import { socket } from 'src/app/service/socket';
 
 @Component({
   selector: 'app-listas',
@@ -11,8 +12,17 @@ import { ListaService } from 'src/app/service/lista.service';
 export class ListasComponent implements OnInit {
   modal: boolean = false
 
+  hiddenLabel = 'hidden'
+  hiddenCriaLabel = 'hidden'
+  displayMenuUser = 'hidden'
+
+  novoLabel = ''
+  novaCorlabel: string[] = []
+
+  @Input() boardId: string
   @Input() lista: ILista
   @Input() projetos: IProjeto[]
+  @Input() labels: ILabel[]
 
   timeElapsed = Date.now();
   today = new Date(this.timeElapsed);
@@ -22,11 +32,41 @@ export class ListasComponent implements OnInit {
   dataInicio: Date
   dataPrevisao: Date
 
-  displayMenuUser = 'hidden'
+  listaLabelColor: string[][] = [
+    ["#ff0000", "#fff"],
+    ["#ff8400", "#fff"],
+    ["#ffb700", "#fff"],
+    ["#d9ff00", "#000"],
+    ["#6aff00", "#000"],
+    ["#09ff00", "#000"],
+    ["#00ff91", "#000"],
+    ["#00aeff", "#fff"],
+    ["#001eff", "#fff"],
+    ["#6200ff", "#fff"],
+    ["#ea00ff", "#fff"],
+    ["#ff00ae", "#fff"],
+    ["#363636", "#fff"],
+    ["#000000", "#fff"],
+  ]
+
+  teste = "bg-[color:var(--color-list1)]"
+
   
-  constructor(private cardService: CardService, private listaService: ListaService) {}
+  constructor(private cardService: CardService, private listaService: ListaService, private boardService: BoardsService) {}
   
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    socket.on('CriaLabel', label => {
+      console.log(label)
+      this.labels.push(label)
+      const boardJSON = localStorage.getItem('board')
+      if(boardJSON){
+        const local: {id: string, board: IBoard} = JSON.parse(boardJSON)
+        local.board.labels = this.labels
+        this.boardService.AtualizaLocalStorage(local.board)
+      }
+
+    })
+  }
 
   allowDrop($event: DragEvent) {
     $event.preventDefault();
@@ -87,4 +127,47 @@ export class ListasComponent implements OnInit {
     this.listaService.DeletaLista(this.lista._id)
   }
 
+  MenuSetLabel(){
+    if(this.hiddenLabel == 'hidden'){
+      this.hiddenLabel = ''
+    } else {
+      this.hiddenLabel = 'hidden'
+    }
+  }
+
+  MenuCriaLabel(){
+    this.MenuSetLabel()
+    if(this.hiddenCriaLabel == 'hidden'){
+      this.hiddenCriaLabel = ''
+    } else {
+      this.hiddenCriaLabel = 'hidden'
+    }
+  }
+
+  modelChangeFn(e:any) {
+    this.novoLabel = e;
+  }
+
+  SetLabel(cor: string[]){
+    this.novaCorlabel = cor
+  }
+
+  CriaLabel() {
+    if(this.novaCorlabel.length && this.novoLabel){
+      this.boardService.CriaLabel(this.novoLabel, this.novaCorlabel[0], this.boardId).subscribe(res => {
+        this.labels.push(res as ILabel)
+      }, err => {
+
+      })
+    }
+  }
+
+  getCorText(cor: string): string{
+    for(let i = 0; i < this.listaLabelColor.length; i++){
+      if(this.listaLabelColor[i][0] == cor){
+        return this.listaLabelColor[i][1]
+      }
+    }
+    return '#fff'
+  }
 }
